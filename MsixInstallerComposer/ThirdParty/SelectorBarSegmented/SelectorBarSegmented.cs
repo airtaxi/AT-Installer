098652aa65198e8fe42e20a03930be79333208e5
Howon Lee <kck4156@kagamine-rin.com>
2026-09-06 23:33:07 +0900

    fix: add missing using directives for ported SelectorBarSegmented

--- MsixInstallerComposer/ThirdParty/SelectorBarSegmented/SelectorBarSegmented.cs
+++ MsixInstallerComposer/ThirdParty/SelectorBarSegmented/SelectorBarSegmented.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Linq;
 
 namespace MsixInstallerComposer.ThirdParty.SelectorBarSegmented;
 
--- MsixInstallerComposer/ThirdParty/SelectorBarSegmented/SelectorBarSegmentedMarginConverter.cs
+++ MsixInstallerComposer/ThirdParty/SelectorBarSegmented/SelectorBarSegmentedMarginConverter.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MsixInstallerComposer.ThirdParty.SelectorBarSegmented;
 
